c595cbed17d441acfe0547170928021063b04237
Ron Kurr <kurr@kurron.org>
2015-09-21 13:23:26 +0000

    Fixed the bad path to the data file

--- send-request.sh
+++ send-request.sh
@@ -3,6 +3,6 @@
 for i in `seq 1 100`;
 do
     UUID=$(cat /proc/sys/kernel/random/uuid)
-    curl --verbose --data-ascii @bin/request.json --header "X-Correlation-Id: ${UUID}" --header 'Content-Type: application/json' localhost:8000
+    curl --verbose --data-ascii @request.json --header "X-Correlation-Id: ${UUID}" --header 'Content-Type: application/json' localhost:8000
 done
 
